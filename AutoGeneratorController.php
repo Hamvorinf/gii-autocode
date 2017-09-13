255c42a656da0c5f769b399cb849fbdebad7d27f
liaoxiaoqi <liaoxiaoqi@shanxiu365.com>
2017-09-13 11:47:08 +0800

    init model and crud auto generator

--- AutoGeneratorController.php
+++ AutoGeneratorController.php
@@ -18,55 +18,98 @@ use yii\db\Connection;
 class AutoGeneratorController extends Controller
 {
     /**
-     * @var string yii文件所在绝对路径
+     * @var string Yii project path
      */
     public $yiiPath = 'yii';
 
     /**
-     * @var Connection|string 数据库连接|数据库配置
+     * @var Connection|string Database connection
      */
     public $db = 'db';
 
     /**
-     * @var string Model命名空间
+     * @var string Database connection name
      */
-    public $modelNs = 'common\\\\models\\\\base';
+    private $dbName = 'db';
 
     /**
-     * @var string Controller命名空间
+     * @var boolean whether the strings will be generated using `Yii::t()` or normal strings.
      */
-    public $ctlNs = 'backend\\\\controller';
+    public $enableI18N = true;
 
     /**
-     * @var int 是否使用表前缀
+     * @var string the message category used by `Yii::t()` when `$enableI18N` is `true`.
+     * Defaults to `app`.
      */
-    public $useTablePrefix = 1;
+    public $messageCategory = 'app';
 
     /**
-     * @var int 是否使用表注释生成字段说明
+     * @var array Model generator config
      */
-    public $generateLabelsFromComments = 1;
+    public $modelConfig;
 
     /**
-     * 初始化
+     * @var array Model generator default config
+     */
+    private $defaultModelConfig = [
+        'ns' => 'common\\\\models\\\\base',             // model namespace
+        'baseClass' => 'yii\\\\db\\\\ActiveRecord',     // model template class
+        'useTablePrefix' => 1,                          // whether use table prefix
+        'generateLabelsFromComments' => 1,              // whether use comments of tables as label annotations
+    ];
+
+    /**
+     * @var array Controller generator config
+     */
+    public $controllerConfig;
+
+    /**
+     * @var array Controller generator default config
+     */
+    public $standarControllerConfig = [
+        'ns' => 'backend\\\\controller',                // controller namespace
+        'viewPath' => '@backend/views',                // view file path
+        'baseClass' => 'yii\\\\web\\\\Controller',      // controller template class
+    ];
+
+
+    /**
+     * Initialization
+     * @throws InvalidConfigException
      */
     public function init()
     {
+        // check db connection
         if (is_string($this->db)) {
+            $this->dbName = $this->db;
             $this->db = Yii::$app->get($this->db);
         }
 
-        //检查yii路径
+        // check yii project path
         $this->yiiPath = realpath($this->yiiPath);
         if($this->yiiPath === false){
-            throw new InvalidConfigException('错误的路径: '.$this->yiiPath.' 请使用绝对路径');
+            throw new InvalidConfigException('Wrong path: '.$this->yiiPath.'. Please set the absolute path.');
+        }
+
+        // check model config
+        foreach($this->defaultModelConfig as $k => $v) {
+            if(!isset($this->modelConfig[$k])) {
+                $this->modelConfig[$k] = $v;
+            }
+        }
+
+        // check controller config
+        foreach($this->standarControllerConfig as $k => $v) {
+            if(!isset($this->controllerConfig[$k])) {
+                $this->controllerConfig[$k] = $v;
+            }
         }
 
         parent::init();
     }
 
     /**
-     * 执行命令
+     * Run
      * @return int
      */
     public function actionIndex()
@@ -75,34 +118,58 @@ class AutoGeneratorController extends Controller
         $return = null;
         $tables = $this->db->createCommand('show tables')->queryAll();
         if (count($tables) == 0) {
-            echo "don't have anything \n";
+            echo "No any tables.\n";
             return 0;
         }
 
         foreach ($tables as &$table) {
             $table = current($table);
-            $class = str_replace(' ', '', ucwords(str_replace('_', ' ', str_replace($this->db->tablePrefix, '', $table))));
-            $cmd = 'php ' . $this->yiiPath . ' gii/model '
+            $modelClass = str_replace(' ', '', ucwords(str_replace('_', ' ', str_replace($this->db->tablePrefix, '', $table))));
+            $cmdModel = 'php ' . $this->yiiPath . ' gii/model '
                 . '--tableName=' . $table . ' '
-                . '--modelClass=' . $class . ' '
-                . '--ns=' . $this->modelNs . ' '
-                . '--useTablePrefix=' . $this->useTablePrefix . ' '
-                . '--generateLabelsFromComments=' . $this->generateLabelsFromComments . ' ';
+                . '--modelClass=' . $modelClass . ' '
+                . '--db=' . $this->dbName . ' '
+                . '--ns=' . $this->modelConfig['ns'] . ' '
+                . '--useTablePrefix=' . $this->modelConfig['useTablePrefix'] . ' '
+                . '--generateLabelsFromComments=' . $this->modelConfig['generateLabelsFromComments'] . ' '
+                . '--baseClass=' . $this->modelConfig['baseClass'] . ' '
+                . '--enableI18N=' . $this->enableI18N . ' '
+                . '--messageCategory=' . $this->messageCategory;
 
-            echo "\n" . 'exec: '. $cmd . "\n";
-            exec($cmd . '<<< "ya"', $output, $return);
+            echo "\n\n===> Generoting Model " . $modelClass . " Begin <===\n". $cmdModel . "\n";
+            exec($cmdModel . '<<< "ya"', $output, $return);
+            $this->showLogs($output);
 
-            foreach ($output as $item) {
-                if (empty($item)) {
-                    continue;
-                }
-                echo $item . "\n";
-            }
+            $controllerClass = $modelClass . 'Controller';
+            $viewPath = $this->controllerConfig['viewPath'] . '/' . strtolower(preg_replace('/([A-Z])/', "-$1", lcfirst($modelClass)));
+            $searchModelClass = $modelClass . 'Search';
+            $cmdController = 'php ' . $this->yiiPath . ' gii/crud '
+                . '--modelClass=' . $this->modelConfig['ns'] . '\\\\' . $modelClass . ' '
+                . '--searchModelClass=' . $this->modelConfig['ns'] . '\\\\' . $searchModelClass . ' '
+                . '--controllerClass=' .$this->controllerConfig['ns'] . '\\\\' . $controllerClass . ' '
+                . '--viewPath=' . $viewPath . ' '
+                . '--baseControllerClass=' . $this->controllerConfig['baseClass'] . ' '
+                . '--enableI18N=' . $this->enableI18N . ' '
+                . '--messageCategory=' . $this->messageCategory;
 
-            unset($table, $class, $output, $return);
+            echo "\n===> Generoting CRUD Controller " . $controllerClass . " Begin <===\n". $cmdController . "\n";
+            exec($cmdController . '<<< "ya"', $output, $return);
+            $this->showLogs($output);
+
+
+            unset($table, $modelClass, $output, $return);
         }
 
-        echo "generate done!\n";
+        echo "Generate done!\n";
         return 0;
+    }
+
+    private function showLogs($output) {
+        foreach ($output as $item) {
+            if (empty($item)) {
+                continue;
+            }
+            echo $item . "\n";
+        }
     }
 } 
